1f41d888e45fad41afb4a6548a02ea137b58a5f0
beverts312 <texas312@gmail.com>
2015-11-17 20:22:39 -0600

    added shorthand

--- bin/reigstry.ts
+++ bin/reigstry.ts
@@ -5,6 +5,6 @@ import program = require('commander');
 program
 	.command('compare')
 	.description('Compares image in multiple repositories')
-	.option('--image <image>', 'The name of the image')
-	.option('--tag <tag>', 'The image tag')
+	.option('-i, --image <image>', 'The name of the image')
+	.option('-t, --tag <tag>', 'The image tag')
 	.action(require('./Compare'));
