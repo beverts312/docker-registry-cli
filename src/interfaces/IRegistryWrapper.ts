9f4cfb94c88623a5bb22de4873f330583110f146
beverts312 <texas312@gmail.com>
2015-11-24 21:13:16 -0600

    updated interface

--- src/interfaces/IRegistryWrapper.ts
+++ src/interfaces/IRegistryWrapper.ts
@@ -2,11 +2,11 @@
 import ImageCatalog = require('../models/ImageCatalog');
 import TagsList = require('../models/TagsList');
 import Manifest = require('../models/Manifest');
+import Registry = require('../models/Registry');
 
 interface IRegistryWrapper {
-	url:string;
-	port:number;
-	
+	registry:Registry;
+		
 	getCatalog(callback:(err:Error, result:ImageCatalog)=>void):void;	
 	getTags(image:string, callback:(err:Error, result:TagsList)=>void):void;
 	getManifest(image:string, tag:string, callback:(err:Error, result:Manifest)=>void):void;
