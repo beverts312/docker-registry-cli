bc3b47afcce79fe8ab0bbecdee3ee6f2225af5f3
beverts312 <texas312@gmail.com>
2015-11-28 09:42:22 -0600

    fixed auth

--- src/RegistryWrapper.ts
+++ src/RegistryWrapper.ts
@@ -18,9 +18,7 @@ class RegistryWrapper implements IRegistryWrapper {
 		this.options.host = this.registry.host;
 		this.options.port = this.registry.port;
 		this.options.rejectUnauthorized = false;
-		if(this.registry.user && this.registry.password){
-			this.options.auth = this.registry.user + ':' + this.registry.password;			
-		}
+		this.options.auth = this.registry.user + ':' + this.registry.password;
 	}
 	
 	getCatalog(callback:(err:Error, result:ImageCatalog)=>void){
