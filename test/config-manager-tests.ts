126c860777f07df18e091b6b719fb5d4ede8de7c
beverts312 <texas312@gmail.com>
2016-01-20 21:58:50 -0600

    adding testing

---
+++ test/config-manager-tests.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+import Chai = require('chai');
+import Sinon = require('sinon');
+
+import ConfigManager = require('../src/ConfigManager');
+
+
+var assert = Chai.assert;
+
+describe('ConfigManager Suite -', ()=>{
+    var configManager: ConfigManager;
+    var sandbox;
+    
+    beforeEach(()=>{
+       configManager = new ConfigManager();
+       sandbox = Sinon.sandbox.create(); 
+    });
+    
+    afterEach(()=>{
+       configManager = null;
+       sandbox.restpre(); 
+    });
+    
+    describe('getRegistries -', () =>{
+       before(()=>{
+           // initialize some registries
+       });
+       
+       beforeEach(()=>{
+           // set up stubs
+       }); 
+       
+       it('Should get registries', (done: () => void) => {
+          assert.equal('1','1');
+          done(); 
+       });
+    });
+    
+});
