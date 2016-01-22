f9fd1b7d0e1c627958a84fc02cdbb7a2b9514cc7
beverts312 <texas312@gmail.com>
2016-01-22 10:43:35 -0600

    reorged main prog

--- bin/registry.ts
+++ bin/registry.ts
@@ -6,12 +6,23 @@ import RegistriesManager = require('./registries-manager');
 
 var registriesManager = new RegistriesManager();                           
 
-program.command('compare <image> <tag>')
-	.description('Compares image in multiple repositories')
-	.option('-i, --image <image>', 'The name of the image')
-	.option('-t, --tag <tag>', 'The image tag')
-	.action(require('./Compare'));
-
+//Single Registry
+program.command('catalog')
+	.description('Lists all images in the registry')
+	.option('-r, --registry-name <reg>', 'The name of the registry to use')
+	.action((name, tag) => {
+		var basicOps = new BasicOperations();
+		basicOps.getCatalog();
+	});
+    
+program.command('tags <image>')
+	.description('Lists all tags for an image')
+	.option('-r, --registry-name <reg>', 'The name of the registry to use')
+	.action((image) => {
+		var basicOps = new BasicOperations();
+		basicOps.getTags(image);
+	});  
+    
 program.command('manifest <name> <tag>')
 	.description('Gets image manifest')
 	.option('-r, --registry-name <reg>', 'The name of the registry to use')
@@ -19,7 +30,15 @@ program.command('manifest <name> <tag>')
 		var basicOps = new BasicOperations();
 		basicOps.getManifest(name, tag);
 	});
-	
+    
+//Multi Registry
+program.command('compare <image> <tag>')
+	.description('Compares image in multiple repositories')
+	.option('-i, --image <image>', 'The name of the image')
+	.option('-t, --tag <tag>', 'The image tag')
+	.action(require('./Compare'));
+
+//Config	
 program.command('add <name> <hostname> <port> <user> <password>')
     .description('Adds registry creds to config')
     .option('-u, --user <user>', 'Registry user')
