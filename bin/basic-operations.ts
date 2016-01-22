5ec3786a433c424993a04077272861cc138e2afc
beverts312 <texas312@gmail.com>
2016-01-21 22:03:25 -0600

    renamed, removed proccess command

---
+++ bin/basic-operations.ts
@@ -0,0 +1,56 @@
+'use strict';
+import ImageCatalog = require('../src/models/image-catalog');
+import TagsList = require('../src/models/tags-list');
+import Manifest = require('../src/models/manifest');
+import Registry = require('../src/models/registry');
+import RegistryWrapper =  require('../src/registry-wrapper');
+
+class BasicOperations {
+    wrapper:RegistryWrapper;
+	
+	constructor(wrapper?:RegistryWrapper){
+        if(!wrapper){
+            var config = require('./configuration.json');
+		    var registry = config.registries[config.registry];
+            registry.password = new Buffer(registry.password, 'base64').toString('ascii');
+		    this.wrapper = new RegistryWrapper(registry);
+        }
+		else{
+            this.wrapper = wrapper;
+        }
+	}
+	
+	getCatalog(){
+		this.wrapper.getCatalog((err, res)=>{
+			if(err){
+				console.log(err.message);
+			}
+			else{
+				console.log(res);
+			}
+		});	
+	}
+	
+	getTags(image:string){
+		this.wrapper.getTags(image, (err, res)=>{
+			if(err){
+				console.log(err.message);
+			}
+			else{
+				console.log(res);
+			}	
+		});
+	}
+	
+	getManifest(image:string, tag:string){
+		this.wrapper.getManifest(image, tag, (err, res) =>{
+			if(err){
+				console.log(err.message);
+			}
+			else{
+				console.log(res);
+			}	
+		});
+	}
+}
+export = BasicOperations;
