63d519664b33eca6e0c87345921380f84db40219
beverts312 <texas312@gmail.com>
2016-02-02 23:10:53 -0600

    added wrapper tests

--- test/src/registry-wrapper-tests.ts
+++ test/src/registry-wrapper-tests.ts
@@ -1,46 +1,70 @@
 /// <reference path="../../typings/tsd.d.ts" />
-/*
+
 'use strict';
 
 import Chai = require('chai');
 import Sinon = require('sinon');
-import PassThrough = require('stream').PassThrough;
 import https = require('https');
+var PassThrough = require('stream').PassThrough;
 
 import RegistryWrapper = require('../../src/registry-wrapper');
 import Registry = require('../../src/models/registry');
+import Manifest = require('../../src/models/manifest');
 import Catalog = require('../../src/models/image-catalog');
+import TagsList = require('../../src/models/tags-list');
 
 
 var assert = Chai.assert;
 
 describe('Registry Wrapper Suite -', ()=>{
-    var sandbox;
     var wrapper: RegistryWrapper;
-    
     beforeEach(()=>{
-       sandbox = Sinon.sandbox.create(); 
-	   this.request = Sinon.stub(https, 'request'); 
-       wrapper = new RegistryWrapper(new Registry('','',0,'',''));      
+        this.request = Sinon.stub(https, 'request');
+        wrapper = new RegistryWrapper(new Registry('','',0,'',''));      
     });
     
     afterEach(()=>{
        wrapper = null;
        https.request.restore();        
-       sandbox.restore(); 
     });
     
-    it('test', (done: () => void) => {
-        var res = new Catalog();
-        var response = new PassThrough();
-        response.write(JSON.stringify(res));
-        response.end();
-        var request = new PassThrough();
-        this.request.callsArgWith(1,response).returns(request);
+    it('GetCatalog Test -', (done: () => void) => {
+        var expected = new Catalog();
+		var response = new PassThrough();
+		response.write(JSON.stringify(expected));
+		response.end();
+        var req = new PassThrough();
+        this.request.callsArgWith(1,response).returns(req);
         wrapper.getCatalog((err, result )=>{
-          console.log(JSON.stringify(result));  
+            assert.deepEqual(expected, result);  
+            done();
         });
-        done();
     });
     
-});*/
+    it('GetTags Test -', (done: () => void) => {
+        var expected = new TagsList();
+		var response = new PassThrough();
+		response.write(JSON.stringify(expected));
+		response.end();
+        var req = new PassThrough();
+        this.request.callsArgWith(1,response).returns(req);
+        wrapper.getTags('image',(err, result )=>{
+            assert.deepEqual(expected, result);  
+            done();
+        });
+    });
+    
+    it('GetManifest Test -', (done: () => void) => {
+        var expected = new Manifest();
+		var response = new PassThrough();
+		response.write(JSON.stringify(expected));
+		response.end();
+        var req = new PassThrough();
+        this.request.callsArgWith(1,response).returns(req);
+        wrapper.getManifest('image','tag',(err, result )=>{
+            assert.deepEqual(expected, result);  
+            done();
+        });
+    });
+    
+});
